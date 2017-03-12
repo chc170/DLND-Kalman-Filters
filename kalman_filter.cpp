4ca1f9f7ee62bc11458aff625da046ec48dd0164
Chih-Chuan Chan <dincoco@gmail.com>
2017-03-12 01:08:41 -0500

    Added kalman filter example 2

--- kalman_filter.cpp
+++ kalman_filter.cpp
@@ -1,92 +1,29 @@
-// Write a function 'filter()' that implements a multi-
-// dimensional Kalman Filter for the example given
-//============================================================================
-#include <iostream>
-#include "Eigen/Dense"
-#include <vector>
+#include "kalman_filter.h"
 
-using namespace std;
-using namespace Eigen;
-
-//Kalman Filter variables
-VectorXd x;	// object state
-MatrixXd P;	// object covariance matrix
-VectorXd u;	// external motion
-MatrixXd F; // state transition matrix
-MatrixXd H;	// measurement matrix
-MatrixXd R;	// measurement covariance matrix
-MatrixXd I; // Identity matrix
-MatrixXd Q;	// process covariance matrix
-
-vector<VectorXd> measurements;
-void filter(VectorXd &x, MatrixXd &P);
-
-
-int main() {
-
-	//design the KF with 1D motion
-	x = VectorXd(2);
-	x << 0, 0;
-
-	P = MatrixXd(2, 2);
-	P << 1000, 0, 0, 1000;
-
-	u = VectorXd(2);
-	u << 0, 0;
-
-	F = MatrixXd(2, 2);
-	F << 1, 1, 0, 1;
-
-	H = MatrixXd(1, 2);
-	H << 1, 0;
-
-	R = MatrixXd(1, 1);
-	R << 1;
-
-	I = MatrixXd::Identity(2, 2);
-
-	Q = MatrixXd(2, 2);
-	Q << 0, 0, 0, 0;
-
-	//create a list of measurements
-	VectorXd single_meas(1);
-	single_meas << 1;
-	measurements.push_back(single_meas);
-	single_meas << 2;
-	measurements.push_back(single_meas);
-	single_meas << 3;
-	measurements.push_back(single_meas);
-
-	//call Kalman filter algorithm
-	filter(x, P);
-
-	return 0;
-
+KalmanFilter::KalmanFilter() {
 }
 
-
-void filter(VectorXd &x, MatrixXd &P) {
-
-	for (unsigned int n = 0; n < measurements.size(); ++n) {
-
-		VectorXd z = measurements[n];
-
-		// KF Measurement update step
-		MatrixXd y = z - H * x;
-		MatrixXd S = H * P * H.transpose() + R;
-		MatrixXd K = P * H.transpose() * S.inverse();
-
-		// new state
-		x = x + K * y;
-		P = (I - K * H) * P;
-
-		// KF Prediction step
-		x = F * x + u;
-		P = F * P * F.transpose();
-
-		std::cout << "x=" << std::endl <<  x << std::endl;
-		std::cout << "P=" << std::endl <<  P << std::endl;
-
-	}
+KalmanFilter::~KalmanFilter() {
 }
 
+void KalmanFilter::Predict() {
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
+}
